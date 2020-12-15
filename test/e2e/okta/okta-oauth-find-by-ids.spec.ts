1e51f549b23645186bd23814bd07ee0744eee86b
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2020-12-15 14:52:18 +0000

    Duplicate tests for Okta provider

---
+++ test/e2e/okta/okta-oauth-find-by-ids.spec.ts
@@ -0,0 +1,181 @@
+import nock from 'nock';
+import chai from 'chai';
+import type request from 'superagent';
+
+import UserModel from 'models/user.model';
+import { createUserAndToken } from '../utils/helpers';
+import { closeTestAgent, getTestAgent } from '../utils/test-server';
+import { TOKENS } from '../utils/test.constants';
+import {getMockOktaUser, mockOktaListUsers} from "./okta.mocks";
+
+chai.should();
+
+let requester: ChaiHttp.Agent;
+
+nock.disableNetConnect();
+nock.enableNetConnect(process.env.HOST_IP);
+
+describe('Find users by id', () => {
+
+    before(async () => {
+        if (process.env.NODE_ENV !== 'test') {
+            throw Error(`Running the test suite with NODE_ENV ${process.env.NODE_ENV} may result in permanent data loss. Please use NODE_ENV=test.`);
+        }
+
+        requester = await getTestAgent();
+
+        await UserModel.deleteMany({}).exec();
+    });
+
+    it('Find users without being logged in returns a 401', async () => {
+        const response: request.Response = await requester
+            .post(`/auth/user/find-by-ids`)
+            .send({});
+
+        response.status.should.equal(401);
+    });
+
+    it('Find users while being logged in as a regular user returns a 401 error', async () => {
+        const { token } = await createUserAndToken();
+
+        const response: request.Response = await requester
+            .post(`/auth/user/find-by-ids`)
+            .set('Authorization', `Bearer ${token}`)
+            .send({});
+
+        response.status.should.equal(403);
+        response.body.should.have.property('errors').and.be.an('array');
+        response.body.errors[0].should.have.property('detail').and.equal(`Not authorized`);
+    });
+
+    it('Find users without ids in body returns a 400 error', async () => {
+        const response: request.Response = await requester
+            .post(`/auth/user/find-by-ids`)
+            .set('Authorization', `Bearer ${TOKENS.MICROSERVICE}`)
+            .send({});
+
+        response.status.should.equal(400);
+        response.body.should.have.property('errors').and.be.an('array');
+        response.body.errors[0].should.have.property('detail').and.equal(`Ids objects required`);
+    });
+
+    it('Find users with id list containing non-object ids returns an empty list (invalid ids are ignored)', async () => {
+        mockOktaListUsers({ limit: 100, search: `((profile.legacyId eq "123"))` }, []);
+
+        const response: request.Response = await requester
+            .post(`/auth/user/find-by-ids`)
+            .set('Authorization', `Bearer ${TOKENS.MICROSERVICE}`)
+            .send({ ids: ['123'] });
+
+        response.status.should.equal(200);
+        response.body.should.have.property('data').and.be.an('array').and.length(0);
+    });
+
+    it('Find users with id list containing user that does not exist returns an empty list (empty db)', async () => {
+        mockOktaListUsers({ limit: 100, search: `((profile.legacyId eq "58333dcfd9f39b189ca44c75"))` }, []);
+
+        const response: request.Response = await requester
+            .post(`/auth/user/find-by-ids`)
+            .set('Authorization', `Bearer ${TOKENS.MICROSERVICE}`)
+            .send({ ids: ['58333dcfd9f39b189ca44c75'] });
+
+        response.status.should.equal(200);
+        response.body.should.have.property('data').and.be.an('array').and.length(0);
+    });
+
+    it('Find users with id list containing a user that exists returns only the listed user', async () => {
+        const user = getMockOktaUser();
+        mockOktaListUsers({ limit: 100, search: `((profile.legacyId eq "${user.id}"))` }, [user]);
+
+        const response: request.Response = await requester
+            .post(`/auth/user/find-by-ids`)
+            .set('Authorization', `Bearer ${TOKENS.MICROSERVICE}`)
+            .send({ ids: [user.id] });
+
+        response.status.should.equal(200);
+        response.body.should.have.property('data').and.be.an('array').and.length(1);
+
+        const responseUserOne: Record<string, any> = response.body.data[0];
+
+        responseUserOne.should.have.property('_id').and.equal(user.profile.legacyId);
+        responseUserOne.should.have.property('extraUserData').and.be.an('object');
+        responseUserOne.extraUserData.should.have.property('apps').and.be.an('array').and.deep.equal(user.profile.apps);
+        responseUserOne.should.have.property('email').and.equal(user.profile.email);
+        responseUserOne.should.have.property('createdAt');
+        responseUserOne.should.have.property('role').and.equal(user.profile.role);
+        responseUserOne.should.have.property('provider').and.equal(user.profile.provider);
+    });
+
+    it('Find users with id list containing users that exist returns the listed users', async () => {
+        const userOne = getMockOktaUser();
+        const userTwo = getMockOktaUser();
+        mockOktaListUsers(
+            { limit: 100, search: `((profile.legacyId eq "${userOne.id}") or (profile.legacyId eq "${userTwo.id}"))` },
+            [userOne, userTwo]
+        );
+
+        const response: request.Response = await requester
+            .post(`/auth/user/find-by-ids`)
+            .set('Authorization', `Bearer ${TOKENS.MICROSERVICE}`)
+            .send({ ids: [userOne.id, userTwo.id] });
+
+        response.status.should.equal(200);
+        response.body.should.have.property('data').and.be.an('array').and.length(2);
+
+        const responseUserOne: Record<string, any> = response.body.data[0];
+        const responseUserTwo: Record<string, any> = response.body.data[1];
+
+        responseUserOne.should.have.property('_id').and.equal(userOne.profile.legacyId);
+        responseUserOne.should.have.property('extraUserData').and.be.an('object');
+        responseUserOne.extraUserData.should.have.property('apps').and.be.an('array').and.deep.equal(userOne.profile.apps);
+        responseUserOne.should.have.property('email').and.equal(userOne.profile.email);
+        responseUserOne.should.have.property('createdAt');
+        responseUserOne.should.have.property('role').and.equal(userOne.profile.role);
+        responseUserOne.should.have.property('provider').and.equal(userOne.profile.provider);
+
+        responseUserTwo.should.have.property('_id').and.equal(userTwo.profile.legacyId);
+        responseUserTwo.should.have.property('extraUserData').and.be.an('object');
+        responseUserTwo.extraUserData.should.have.property('apps').and.be.an('array').and.deep.equal(userTwo.profile.apps);
+        responseUserTwo.should.have.property('email').and.equal(userTwo.profile.email);
+        responseUserTwo.should.have.property('createdAt');
+        responseUserTwo.should.have.property('role').and.equal(userTwo.profile.role);
+        responseUserTwo.should.have.property('provider').and.equal(userTwo.profile.provider);
+    });
+
+    it('Find users with id list containing users that exist returns the listed users (id query param is useless)', async () => {
+        const userOne = getMockOktaUser();
+        mockOktaListUsers(
+            { limit: 100, search: `((profile.legacyId eq "${userOne.id}"))` },
+            [userOne]
+        );
+
+        const response: request.Response = await requester
+            .post(`/auth/user/find-by-ids?ids=123333`)
+            .set('Authorization', `Bearer ${TOKENS.MICROSERVICE}`)
+            .send({ ids: [userOne.id] });
+
+        response.status.should.equal(200);
+        response.body.should.have.property('data').and.be.an('array').and.length(1);
+
+        const responseUserOne: Record<string, any> = response.body.data[0];
+
+        responseUserOne.should.have.property('_id').and.equal(userOne.profile.legacyId);
+        responseUserOne.should.have.property('extraUserData').and.be.an('object');
+        responseUserOne.extraUserData.should.have.property('apps').and.be.an('array').and.deep.equal(userOne.profile.apps);
+        responseUserOne.should.have.property('email').and.equal(userOne.profile.email);
+        responseUserOne.should.have.property('createdAt');
+        responseUserOne.should.have.property('role').and.equal(userOne.profile.role);
+        responseUserOne.should.have.property('provider').and.equal(userOne.profile.provider);
+    });
+
+    after(async () => {
+        await UserModel.deleteMany({}).exec();
+        await closeTestAgent();
+    });
+
+    afterEach(() => {
+        if (!nock.isDone()) {
+            throw new Error(`Not all nock interceptors were used: ${nock.pendingMocks()}`);
+        }
+    });
+});
---
+++ test/e2e/okta/okta-oauth-list-users.spec.ts
@@ -0,0 +1,371 @@
+import chai from 'chai';
+import nock from 'nock';
+import type request from 'superagent';
+
+import UserModel, { IUser, UserDocument } from 'models/user.model';
+import { closeTestAgent, getTestAgent } from '../utils/test-server';
+import { createUserAndToken, ensureHasPaginationElements } from '../utils/helpers';
+import { getMockOktaUser, mockOktaListUsers } from "./okta.mocks";
+
+chai.should();
+
+let requester: ChaiHttp.Agent;
+
+nock.disableNetConnect();
+nock.enableNetConnect(process.env.HOST_IP);
+
+describe('List users', () => {
+
+    before(async () => {
+        if (process.env.NODE_ENV !== 'test') {
+            throw Error(`Running the test suite with NODE_ENV ${process.env.NODE_ENV} may result in permanent data loss. Please use NODE_ENV=test.`);
+        }
+
+        requester = await getTestAgent();
+
+        await UserModel.deleteMany({}).exec();
+    });
+
+    it('Visiting /auth/user while not logged in should return a 401 error', async () => {
+        const response: request.Response = await requester
+            .get(`/auth/user`)
+            .set('Content-Type', 'application/json');
+
+        response.status.should.equal(401);
+        response.should.be.json;
+        response.body.should.have.property('errors').and.be.an('array');
+        response.body.errors[0].should.have.property('detail').and.equal(`Not authenticated`);
+    });
+
+    it('Visiting /auth/user while logged in as USER should return a 403 error', async () => {
+        const { token } = await createUserAndToken(null);
+
+        const response: request.Response = await requester
+            .get(`/auth/user`)
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`);
+
+        response.status.should.equal(403);
+        response.should.be.json;
+        response.body.should.have.property('errors').and.be.an('array');
+        response.body.errors[0].should.have.property('detail').and.equal(`Not authorized`);
+    });
+
+    it('Visiting /auth/user while logged in as MANAGER should return a 403 error', async () => {
+        const { token } = await createUserAndToken({ role: 'MANAGER' });
+
+        const response: request.Response = await requester
+            .get(`/auth/user`)
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`);
+
+        response.status.should.equal(403);
+        response.body.errors[0].should.have.property('detail').and.equal(`Not authorized`);
+    });
+
+    it('Visiting /auth/user while logged in as ADMIN should return the list of users - just current user', async () => {
+        const user = getMockOktaUser({});
+        mockOktaListUsers({ limit: 10, search: '((profile.apps eq "rw"))' }, [user]);
+
+        const { token } = await createUserAndToken({ role: 'ADMIN' });
+        const response: request.Response = await requester
+            .get(`/auth/user`)
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`);
+
+        response.status.should.equal(200);
+        response.body.should.have.property('data').and.be.an('array').and.have.length(1);
+        response.body.data[0].should.have.property('id').and.equal(user.profile.legacyId);
+        response.body.data[0].should.have.property('email').and.equal(user.profile.email);
+        response.body.data[0].should.have.property('role').and.equal(user.profile.role);
+        ensureHasPaginationElements(response);
+    });
+
+    it('Visiting /auth/user while logged in as ADMIN should return the list of users - just current user if no other matches the current user\'s apps', async () => {
+        const user = getMockOktaUser({});
+        mockOktaListUsers({ limit: 10, search: '((profile.apps eq "rw"))' }, [user]);
+
+        const { token } = await createUserAndToken({ role: 'ADMIN' });
+        const response: request.Response = await requester
+            .get(`/auth/user`)
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`);
+
+        response.status.should.equal(200);
+        response.body.should.have.property('data').and.be.an('array').and.have.length(1);
+        response.body.data[0].should.have.property('id').and.equal(user.profile.legacyId);
+        response.body.data[0].should.have.property('email').and.equal(user.profile.email);
+        response.body.data[0].should.have.property('role').and.equal(user.profile.role);
+        ensureHasPaginationElements(response);
+    });
+
+    it('Visiting /auth/user while logged in as ADMIN should return the list of users - only return users that match current user\'s app', async () => {
+        const users = [
+            getMockOktaUser({ email: 'rw-user-one@example.com', apps: ['rw'] }),
+            getMockOktaUser({ email: 'rw-user-two@example.com', apps: ['rw'] }),
+            getMockOktaUser({}),
+        ];
+        mockOktaListUsers({ limit: 10, search: '((profile.apps eq "rw"))' }, users);
+
+        const { token } = await createUserAndToken({ role: 'ADMIN' });
+        const response: request.Response = await requester
+            .get(`/auth/user`)
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`);
+
+        response.status.should.equal(200);
+        response.body.should.have.property('data').and.be.an('array').and.have.length(3);
+        response.body.data.map((e: IUser) => e.email).should
+            .include(users[0].profile.email).and.to
+            .include(users[1].profile.email).and.to
+            .include(users[2].profile.email);
+        ensureHasPaginationElements(response);
+    });
+
+    it('Visiting /auth/user while logged in as ADMIN should return the list of users - filter by email address is supported', async () => {
+        const user = getMockOktaUser({});
+        mockOktaListUsers({ limit: 10, search: `(profile.email sw "${user.profile.email}") and ((profile.apps eq "rw"))` }, [user]);
+
+        const { token } = await createUserAndToken({ role: 'ADMIN' });
+        const response: request.Response = await requester
+            .get(`/auth/user?email=${user.profile.email}`)
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`);
+
+        response.status.should.equal(200);
+        response.body.should.have.property('data').and.be.an('array').and.have.length(1);
+        response.body.data.map((e: IUser) => e.email).should.include(user.profile.email);
+        ensureHasPaginationElements(response);
+    });
+
+    it('Visiting /auth/user while logged in as ADMIN should return the list of users - filter by email address with plus sign in it is supported as long as it\'s escaped', async () => {
+        const user = getMockOktaUser({ email: 'text+email@vizzuality.com' });
+        mockOktaListUsers({ limit: 10, search: `(profile.email sw "${user.profile.email}") and ((profile.apps eq "rw"))` }, [user]);
+
+        const { token } = await createUserAndToken({ role: 'ADMIN', email: 'text+email@vizzuality.com' });
+        const response: request.Response = await requester
+            .get(`/auth/user`)
+            .query({ email: user.profile.email })
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`);
+
+        response.status.should.equal(200);
+        response.body.should.have.property('data').and.be.an('array').and.have.length(1);
+        response.body.data.map((e: UserDocument) => e.email).should.include(user.profile.email);
+
+        ensureHasPaginationElements(response);
+    });
+
+    it('Visiting /auth/user while logged in as ADMIN should return the list of users - filter by provider is supported', async () => {
+        const localUser = getMockOktaUser({ provider: 'local' });
+        const googleUser = getMockOktaUser({ provider: 'google' });
+        mockOktaListUsers(
+            { limit: 10, search: `(profile.provider eq "${localUser.profile.provider}") and ((profile.apps eq "rw"))` },
+            [localUser],
+        );
+
+        const { token } = await createUserAndToken({ role: 'ADMIN' });
+        const responseOne: request.Response = await requester
+            .get(`/auth/user?provider=local`)
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`);
+
+        responseOne.status.should.equal(200);
+        responseOne.body.should.have.property('data').and.be.an('array').and.have.length(1);
+        responseOne.body.data.map((e: IUser) => e.email).should.include(localUser.profile.email);
+        ensureHasPaginationElements(responseOne);
+
+        mockOktaListUsers(
+            { limit: 10, search: `(profile.provider eq "${googleUser.profile.provider}") and ((profile.apps eq "rw"))` },
+            [googleUser],
+        );
+
+        const responseTwo: request.Response = await requester
+            .get(`/auth/user?provider=google`)
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`);
+
+        responseTwo.status.should.equal(200);
+        responseTwo.body.should.have.property('data').and.be.an('array').and.have.length(1);
+        responseTwo.body.data.map((e: IUser) => e.email).should.include(googleUser.profile.email);
+        ensureHasPaginationElements(responseTwo);
+    });
+
+    it('Visiting /auth/user while logged in as ADMIN should return the list of users - filter by name is supported', async () => {
+        const userOne = getMockOktaUser({});
+        const userTwo = getMockOktaUser({});
+
+        mockOktaListUsers(
+            { limit: 10, search: `(profile.displayName sw "${userOne.profile.displayName}") and ((profile.apps eq "rw"))` },
+            [userOne],
+        );
+
+        const { token } = await createUserAndToken({ role: 'ADMIN' });
+        const responseOne: request.Response = await requester
+            .get(`/auth/user?name=${userOne.profile.displayName}`)
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`);
+
+        responseOne.status.should.equal(200);
+        responseOne.body.should.have.property('data').and.be.an('array').and.have.length(1);
+        responseOne.body.data.map((e: IUser) => e.email).should.include(userOne.profile.email);
+        ensureHasPaginationElements(responseOne);
+
+        mockOktaListUsers(
+            { limit: 10, search: `(profile.displayName sw "${userTwo.profile.displayName}") and ((profile.apps eq "rw"))` },
+            [userTwo],
+        );
+
+        const responseTwo: request.Response = await requester
+            .get(`/auth/user?name=${userTwo.profile.displayName}`)
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`);
+
+        responseTwo.status.should.equal(200);
+        responseTwo.body.should.have.property('data').and.be.an('array').and.have.length(1);
+        responseTwo.body.data.map((e: IUser) => e.email).should.include(userTwo.profile.email);
+        ensureHasPaginationElements(responseTwo);
+    });
+
+    it('Visiting /auth/user while logged in as ADMIN should return the list of users - filter by role is supported', async () => {
+        const user = getMockOktaUser({ role: 'USER' });
+        const manager = getMockOktaUser({ role: 'MANAGER' });
+        const admin = getMockOktaUser({ role: 'ADMIN' });
+
+        mockOktaListUsers(
+            { limit: 10, search: `(profile.role eq "${user.profile.role}") and ((profile.apps eq "rw"))` },
+            [user],
+        );
+
+        const { token } = await createUserAndToken({ role: 'ADMIN' });
+        const responseOne: request.Response = await requester
+            .get(`/auth/user?role=USER`)
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`);
+
+        responseOne.status.should.equal(200);
+        responseOne.body.should.have.property('data').and.be.an('array').and.have.length(1);
+        responseOne.body.data.map((e: IUser) => e.email).should.include(user.profile.email);
+        ensureHasPaginationElements(responseOne);
+
+        mockOktaListUsers(
+            { limit: 10, search: `(profile.role eq "${manager.profile.role}") and ((profile.apps eq "rw"))` },
+            [manager],
+        );
+
+        const responseTwo: request.Response = await requester
+            .get(`/auth/user?role=MANAGER`)
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`);
+
+        responseTwo.status.should.equal(200);
+        responseTwo.body.should.have.property('data').and.be.an('array').and.have.length(1);
+        responseTwo.body.data.map((e: IUser) => e.email).should.include(manager.profile.email);
+        ensureHasPaginationElements(responseTwo);
+
+        mockOktaListUsers(
+            { limit: 10, search: `(profile.role eq "${admin.profile.role}") and ((profile.apps eq "rw"))` },
+            [admin],
+        );
+
+        const responseThree: request.Response = await requester
+            .get(`/auth/user?role=ADMIN`)
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`);
+
+        responseThree.status.should.equal(200);
+        responseThree.body.should.have.property('data').and.be.an('array').and.have.length(1);
+        responseThree.body.data.map((e: IUser) => e.email).should.include(admin.profile.email);
+        ensureHasPaginationElements(responseThree);
+    });
+
+    it('Visiting /auth/user while logged in as ADMIN should return the list of users - filter by password not supported', async () => {
+        const user = getMockOktaUser({});
+        mockOktaListUsers({ limit: 10, search: `((profile.apps eq "rw"))` }, [user]);
+
+        const { token } = await createUserAndToken({ role: 'ADMIN' });
+        const response: request.Response = await requester
+            .get(`/auth/user?password=%242b%2410%241wDgP5YCStyvZndwDu2GwuC6Ie9wj7yRZ3BNaaI.p9JqV8CnetdPK`)
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`);
+
+        response.status.should.equal(200);
+        response.body.should.have.property('data').and.be.an('array').and.have.length(1);
+        response.body.data.map((e: IUser) => e.email).should.include(user.profile.email);
+        ensureHasPaginationElements(response);
+    });
+
+    it('Visiting /auth/user while logged in as ADMIN and query app=all should return the list of users - even if apps of users are not match to current user\'s app', async () => {
+        const userOne = getMockOktaUser({ apps: ['gfw'] });
+        const userTwo = getMockOktaUser({ apps: ['rw'] });
+        const userThree = getMockOktaUser({ apps: ['fake-app-2'] });
+        mockOktaListUsers({ limit: 10 }, [userOne, userTwo, userThree]);
+
+        const { token } = await createUserAndToken({ role: 'ADMIN' });
+        const response: request.Response = await requester
+            .get(`/auth/user?app=all`)
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`)
+            .send();
+
+        response.status.should.equal(200);
+        response.body.should.have.property('data').and.be.an('array').and.have.length(3);
+        response.body.data.map((e: UserDocument) => e.email).should
+            .include(userOne.profile.email).and.to
+            .include(userTwo.profile.email).and.to
+            .include(userThree.profile.email);
+        ensureHasPaginationElements(response);
+    });
+
+    it('Visiting /auth/user while logged in as ADMIN and filtering by app should return the list of users with apps which provided in the query app', async () => {
+        const userOne = getMockOktaUser({ apps: ['fake-app'] });
+        const userTwo = getMockOktaUser({ apps: ['fake-app-2'] });
+        mockOktaListUsers(
+            { limit: 10, search: `((profile.apps eq "fake-app") or (profile.apps eq "fake-app-2"))` },
+            [userOne, userTwo]
+        );
+
+        const { token } = await createUserAndToken({ role: 'ADMIN' });
+        const response: request.Response = await requester
+            .get(`/auth/user?app=fake-app,fake-app-2`)
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`)
+            .send();
+
+        response.status.should.equal(200);
+        response.body.should.have.property('data').and.be.an('array').and.have.length(2);
+        response.body.data.map((e: IUser) => e.extraUserData.apps[0]).should
+            .include(userOne.profile.apps[0]).and.to
+            .include(userTwo.profile.apps[0]);
+        ensureHasPaginationElements(response);
+    });
+
+    it('Visiting /auth/user while logged in as ADMIN and an invalid query param should return the list of users ignoring the invalid query param', async () => {
+        const userOne = getMockOktaUser({});
+        const userTwo = getMockOktaUser({});
+        mockOktaListUsers({ limit: 10, search: `((profile.apps eq "rw"))` }, [userOne, userTwo]);
+
+        const { token } = await createUserAndToken({ role: 'ADMIN' });
+        const response: request.Response = await requester
+            .get(`/auth/user?foo=bar`)
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`)
+            .send();
+
+        response.status.should.equal(200);
+        response.body.should.have.property('data').and.be.an('array').and.have.length(2);
+        ensureHasPaginationElements(response);
+    });
+
+    after(() => {
+        closeTestAgent();
+    });
+
+    afterEach(async () => {
+        await UserModel.deleteMany({}).exec();
+
+        if (!nock.isDone()) {
+            throw new Error(`Not all nock interceptors were used: ${nock.pendingMocks()}`);
+        }
+    });
+});
