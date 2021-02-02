2ea09a04274cfa0e30855bc3c909ffc2d6a00828
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2021-02-02 12:21:46 +0000

    Tighten up test cases for authorization code callback endpoint

--- test/e2e/okta/okta-oauth-authorization-code-callback.spec.ts
+++ test/e2e/okta/okta-oauth-authorization-code-callback.spec.ts
@@ -5,9 +5,10 @@ import JWT from 'jsonwebtoken';
 import { closeTestAgent, getTestAgent } from '../utils/test-server';
 import type request from 'superagent';
 import sinon, {SinonSandbox} from 'sinon';
-import {stubConfigValue} from '../utils/helpers';
+import {getUUID, stubConfigValue} from '../utils/helpers';
 import {getMockOktaUser, mockGetUserByOktaId, mockOktaOAuthToken} from './okta.mocks';
-import {OktaOAuthTokenPayload, OktaSuccessfulOAuthTokenResponse, OktaUser} from 'services/okta.interfaces';
+import {JWTPayload, OktaOAuthTokenPayload, OktaSuccessfulOAuthTokenResponse, OktaUser} from 'services/okta.interfaces';
+import config from 'config';
 
 chai.should();
 
@@ -16,6 +17,18 @@ let sandbox: SinonSandbox;
 
 nock.disableNetConnect();
 nock.enableNetConnect(process.env.HOST_IP);
+
+const validateTokenRequestAndaPayload: (user: OktaUser) => Promise<void> = async (user) => {
+    const tokenResponse: request.Response = await requester.get(`/auth/generate-token`);
+    tokenResponse.body.should.have.property('token').and.be.a.string;
+    const tokenPayload: JWTPayload = JWT.decode(tokenResponse.body.token) as JWTPayload;
+    tokenPayload.should.have.property('id').and.eql(user.profile.legacyId);
+    tokenPayload.should.have.property('role').and.eql('USER');
+    tokenPayload.should.have.property('email').and.eql(user.profile.email);
+    tokenPayload.should.have.property('extraUserData').and.eql({ apps: user.profile.apps });
+    tokenPayload.should.have.property('photo').and.eql(user.profile.photo);
+    tokenPayload.should.have.property('name').and.eql(user.profile.displayName);
+};
 
 describe('[OKTA] Authorization code callback endpoint tests', () => {
 
@@ -45,8 +58,8 @@ describe('[OKTA] Authorization code callback endpoint tests', () => {
     it('Visiting /auth/authorization-code/callback with a valid OAuth code should redirect to the login successful page', async () => {
         const tokenResponse: OktaSuccessfulOAuthTokenResponse = mockOktaOAuthToken();
         const tokenData: OktaOAuthTokenPayload = JWT.decode(tokenResponse.access_token) as OktaOAuthTokenPayload;
-        const userData: OktaUser = getMockOktaUser();
-        mockGetUserByOktaId(tokenData.uid, userData);
+        const user: OktaUser = getMockOktaUser();
+        mockGetUserByOktaId(tokenData.uid, user);
 
         const response: request.Response = await requester
             .get(`/auth/authorization-code/callback?code=TEST_FACEBOOK_OAUTH2_CALLBACK_CODE`)
@@ -54,13 +67,15 @@ describe('[OKTA] Authorization code callback endpoint tests', () => {
 
         response.should.redirect;
         response.should.redirectTo(new RegExp(`/auth/success$`));
+
+        await validateTokenRequestAndaPayload(user);
     });
 
     it('Visiting /auth/facebook/callback with a valid OAuth code with a callbackUrl param should redirect to the callback URL page', async () => {
         const tokenResponse: OktaSuccessfulOAuthTokenResponse = mockOktaOAuthToken();
         const tokenData: OktaOAuthTokenPayload = JWT.decode(tokenResponse.access_token) as OktaOAuthTokenPayload;
-        const userData: OktaUser = getMockOktaUser();
-        mockGetUserByOktaId(tokenData.uid, userData);
+        const user: OktaUser = getMockOktaUser();
+        mockGetUserByOktaId(tokenData.uid, user);
 
         nock('https://www.wikipedia.org')
             .get('/')
@@ -78,13 +93,15 @@ describe('[OKTA] Authorization code callback endpoint tests', () => {
         const responseTwo: request.Response = await requester.get('/auth/success');
         responseTwo.should.redirect;
         responseTwo.should.redirectTo('https://www.wikipedia.org/');
+
+        await validateTokenRequestAndaPayload(user);
     });
 
     it('Visiting /auth/facebook/callback with a valid OAuth code with an updated callbackUrl param should redirect to the new callback URL page', async () => {
         const tokenResponse: OktaSuccessfulOAuthTokenResponse = mockOktaOAuthToken();
         const tokenData: OktaOAuthTokenPayload = JWT.decode(tokenResponse.access_token) as OktaOAuthTokenPayload;
-        const userData: OktaUser = getMockOktaUser();
-        mockGetUserByOktaId(tokenData.uid, userData);
+        const user: OktaUser = getMockOktaUser();
+        mockGetUserByOktaId(tokenData.uid, user);
 
         nock('https://www.wri.org')
             .get('/')
@@ -104,13 +121,15 @@ describe('[OKTA] Authorization code callback endpoint tests', () => {
         const responseTwo: request.Response = await requester.get('/auth/success');
         responseTwo.should.redirect;
         responseTwo.should.redirectTo('https://www.wri.org/');
+
+        await validateTokenRequestAndaPayload(user);
     });
 
     it('Visiting /auth/authorization-code/callback with a valid OAuth code should redirect to the login successful page', async () => {
         const tokenResponse: OktaSuccessfulOAuthTokenResponse = mockOktaOAuthToken();
         const tokenData: OktaOAuthTokenPayload = JWT.decode(tokenResponse.access_token) as OktaOAuthTokenPayload;
-        const userData: OktaUser = getMockOktaUser();
-        mockGetUserByOktaId(tokenData.uid, userData);
+        const user: OktaUser = getMockOktaUser();
+        mockGetUserByOktaId(tokenData.uid, user);
 
         const response: request.Response = await requester
             .get(`/auth/authorization-code/callback?code=TEST_FACEBOOK_OAUTH2_CALLBACK_CODE`)
@@ -118,6 +137,50 @@ describe('[OKTA] Authorization code callback endpoint tests', () => {
 
         response.should.redirect;
         response.should.redirectTo(new RegExp(`/auth/success$`));
+
+        await validateTokenRequestAndaPayload(user);
+    });
+
+    it('If the user returned does not have legacyId, role or apps set, the user is updated and the request is still redirected to the login successful page', async () => {
+        const token: OktaSuccessfulOAuthTokenResponse = mockOktaOAuthToken();
+        const tokenData: OktaOAuthTokenPayload = JWT.decode(token.access_token) as OktaOAuthTokenPayload;
+        const user: OktaUser = getMockOktaUser({ legacyId: null, role: null, apps: null });
+        mockGetUserByOktaId(tokenData.uid, user);
+
+        const legacyId: string = getUUID();
+
+        // Mock update of protected user fields
+        nock(config.get('okta.url'))
+            .post(`/api/v1/users/${user.id}`, (body) => !!body.profile.legacyId
+                && body.profile.role === 'USER'
+                && !!body.profile.apps
+            )
+            .reply(200, {
+                ...user,
+                profile: {
+                    ...user.profile,
+                    legacyId,
+                    role: 'USER',
+                    apps: [],
+                }
+            });
+
+        const response: request.Response = await requester
+            .get(`/auth/authorization-code/callback?code=TEST_FACEBOOK_OAUTH2_CALLBACK_CODE`)
+            .redirects(0);
+
+        response.should.redirect;
+        response.should.redirectTo(new RegExp(`/auth/success$`));
+
+        await validateTokenRequestAndaPayload({
+            ... user,
+            profile: {
+                ...user.profile,
+                legacyId,
+                role: 'USER',
+                apps: [],
+            }
+        });
     });
 
     afterEach(async () => {
